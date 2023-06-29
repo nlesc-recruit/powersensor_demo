cde3a47b46b1d4564f55053bdad7bd71a610deb9
Leon Oostrum <l.oostrum@esciencecenter.nl>
2023-06-29 14:27:47 +0200

    Generate header from python script; calculate max_shift based on other params

--- dedispersion.py
+++ dedispersion.py
@@ -8,8 +8,6 @@ from kernel_tuner.observers.powersensor import PowerSensorObserver
 nr_dms = 2048
 nr_samples = 25000
 nr_channels = 1536
-max_shift = 650
-nr_samples_per_channel = (nr_samples+max_shift)
 down_sampling = 1
 dm_first = 0.0
 dm_step = 0.02
@@ -19,45 +17,69 @@ sampling_time = 0.00004096
 min_freq = 1425.0
 max_freq = min_freq + (nr_channels-1) * channel_bandwidth
 
+HEADER_TEMPLATE = """
+#define nr_dms {nr_dms}
+#define nr_samples {nr_samples}
+#define nr_channels {nr_channels}
+#define max_shift {max_shift}
+#define nr_samples_per_channel ({nr_samples} + {max_shift})
+#define dm_first {dm_first}f
+#define dm_step {dm_step}f
+"""
+
+
+def get_shift(freq):
+    inverse_high_freq = max_freq**-2
+    inverse_freq = freq**-2
+    # 4148.808 is the time delay per dispersion measure, a constant in the dispersion equation
+    shift = 4148.808 * (inverse_freq - inverse_high_freq) / (sampling_time * down_sampling)
+    return shift
+
+
+max_shift = int(np.ceil(get_shift(min_freq) * (dm_first + nr_dms * dm_step))) + 1
+nr_samples_per_channel = (nr_samples+max_shift)
 
 
 def get_shifts():
-    max_freq = min_freq + ((nr_channels - 1) * channel_bandwidth)
-    inverse_high_freq = 1/max_freq**2
-    time_unit = nr_samples * sampling_time
-
     channels = np.arange(nr_channels, dtype=np.float32)
-    inverse_freq = 1.0 / (min_freq + (channels * channel_bandwidth))**2
-    # 4148.808 is the time delay per dispersion measure, a constant in the dispersion equation
-    shifts_float = (4148.808 * (inverse_freq - inverse_high_freq) * (nr_samples / down_sampling)) / time_unit
+    freqs = min_freq + (channels * channel_bandwidth)
+    shifts_float = get_shift(freqs)
     shifts_float[-1] = 0
     return shifts_float
 
 
+def write_header():
+    header = HEADER_TEMPLATE.format(nr_dms=nr_dms,
+                                    nr_samples=nr_samples,
+                                    nr_channels=nr_channels,
+                                    max_shift=max_shift,
+                                    dm_first=dm_first,
+                                    dm_step=dm_step)
+    with open("dedispersion.h", "w") as fp:
+        fp.write(header)
+
 
 def create_reference():
+    write_header()
 
-    input_samples = np.random.randn(nr_samples_per_channel*nr_channels).astype(np.uint8)
-
-    output_arr = np.zeros(nr_dms*nr_samples, dtype=np.float32)
+    input_samples = np.random.randn(nr_samples_per_channel * nr_channels).astype(np.uint8)
+    output_arr = np.zeros(nr_dms * nr_samples, dtype=np.float32)
     shifts = get_shifts()
 
     kernel_name = "dedispersion_reference"
     kernel_string = "dedispersion.cc"
     args = [input_samples, output_arr, shifts]
 
-    reference = kt.run_kernel("dedispersion_reference", "dedispersion.cc", 1, args, {}, lang="C")
-
-    print(reference[1][:50])
+    reference = kt.run_kernel(kernel_name, kernel_string, 1, args, {}, lang="C")
 
     np.save("input_ref", input_samples, allow_pickle=False)
     np.save("shifts_ref", shifts, allow_pickle=False)
     np.save("dedisp_ref", reference[1], allow_pickle=False)
 
-def tune():
 
+def tune():
     input_samples = np.load("input_ref.npy")
-    output_arr = np.zeros(nr_dms*nr_samples, dtype=np.float32)
+    output_arr = np.zeros(nr_dms * nr_samples, dtype=np.float32)
     shifts = np.load("shifts_ref.npy")
 
     # ensure consistency of the input files
@@ -83,7 +105,6 @@ def tune():
     [print(k,v) for k,v in tune_params.items()]
 
     cp = [f"-I{os.path.dirname(os.path.realpath(__file__))}"]
-
 
     check_block_size = "32 <= block_size_x * block_size_y <= 1024"
     #check_loop_x = "loop_unroll_factor_x <= tile_size_x and tile_size_x % loop_unroll_factor_x == 0"
@@ -111,13 +132,9 @@ def tune():
 
 
 
-
-
-
-
 if __name__ == "__main__":
+    print("Creating reference ...")
+    create_reference()
 
-    print("Tuning ... ")
-    tune()
-
-
+    # print("Tuning ...")
+    # tune()
